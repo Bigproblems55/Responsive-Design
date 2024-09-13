592c59e2247414ca5d3ca06a7f39ac8269b77fab
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-13 09:33:37 -0700

    Styling needs to be rewritten. Something is over writing my border-radius for circular buttons

--- Portfolio/src/Homepath/Home.jsx
+++ Portfolio/src/Homepath/Home.jsx
@@ -2,14 +2,22 @@ import React,{useState} from "react";
 import Mydf from '../../Data/Nicholas Anthony Lacapria pdf resume.pdf';
 export default function Home(){
 
- 
+    function handleIframe(){
+
+        if(window.matchMedia("all and (min-width: 680px)") >= 680){
+            console.log(window.matchMedia("all and (min-width: 680px)"))
+            return(
+                <iframe src={Mydf} 
+        width="100%" 
+        height="100%" />
+            )
+        }
+    }
     return(
         <>
-            {
-                <iframe src={Mydf} 
-                width="100%" 
-                height="100%" />
-            }
+            <iframe src={Mydf} 
+        width="100%" 
+        height="100%" />
         </>
     )
 }
