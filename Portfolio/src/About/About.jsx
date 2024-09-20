ca9866627ae3e9126c3ab3b96d35a9a29d769947
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-20 12:57:10 -0700

    styling is ok for now the project boxes will need some touchup for the image and margins

--- Portfolio/src/About/About.jsx
+++ Portfolio/src/About/About.jsx
@@ -4,10 +4,10 @@ export default function  About(){
 
     return(
         <>
-           <div className='abcon'>
+           <div className='contactAbout'>
              <h1 className='abconh1'>About Nicholas Lacapria</h1>
             <p >
-                I am a full stack webdeveoper. I enjoy node.js as backend and React for the front-end. I have been programming in general with other languages since 2012 but I have specialized in Mern stack development since 2018 and I am seeking a job in this field currently. If anyone is looking for someone dedicated enough to for development... I am available immediatly and I am willing to relocate if I have to. I work on projects all the time to better myself and I am constantly updating old applications so they adhear to the new standards. I enjoy solving problems and would be greatful to be apart of a team that does what I am learning so I am able to do  it professionally while making money and doing what I like. I do have a github where you can see my profile and organizations I have worked with. A major one has been with the school of Lambda who taught me alot about the field and the way application programming works with react and Nodejs. My githubs are viewable here:      
+                I am a full stack webdeveoper using Mernstack Technology. I enjoy node.js as backend and React for the front-end. I have been programming in general with other languages since 2012 but I have specialized in Mern stack development since 2018 and I am seeking a job in this field currently. If anyone is looking for someone dedicated enough to for development... I am available immediatly and I am willing to relocate if I have to. I work on projects all the time to better myself and I am constantly updating old applications so they adhear to the new standards. I enjoy solving problems and would be greatful to be apart of a team that does what I am learning so I am able to do  it professionally while making money and doing what I like. I do have a github where you can see my profile and organizations I have worked with. A major one has been with the school of Lambda who taught me alot about the field and the way application programming works with react and Nodejs. My githubs are viewable here:      
             </p>
             <div className='abcon1'>
                 <p >
--- Portfolio/src/Contact/Contact.jsx
+++ Portfolio/src/Contact/Contact.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import googleMail from '../Assets/gmail.png'
+import linkd from '../Assets/linkedin.png'
 export default function Contact(){
 
     return(
-        <div >
-            <h1>Contact Me</h1>
-            <i class="fa fa-google" aria-hidden="true"><a href='Nicholas.a.lacapria@gmail.com '>mail </a></i> 
-            <h1> or </h1>
-            <i class="fa fa-linkedin" aria-hidden="true"><a href='https://www.linkedin.com/web-dev-nicholas-lacapria'> Linkedin</a></i>
-        </div>
+        <div className='contactAbout'>
+            <div id="contactme">
+            <h1 className='h1style'>Contact Me</h1>
+            <a href='Nicholas.a.lacapria@gmail.com '><img src={googleMail} />Nicholas.a.lacapria@gmail.com </a>
+            <h1 className='h1style'> or </h1>
+            
+            <a href='https://www.linkedin.com/in/web-dev-nicholas-lacapria'>
+            <img src={linkd} />linkedin.com/in/web-dev-nicholas-lacapria</a>
+        
+            </div>
+            </div>
     )
 }
