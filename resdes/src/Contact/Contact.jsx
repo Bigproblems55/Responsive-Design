8e1e1a4da3d2571063cc72f3539388737932ca5e
Rivit Not Vivit <jack.problems@protonmail.com>
2025-12-13 08:00:19 -0800

    Changes to contact information to prevent scammers from emailing me

--- resdes/src/Contact/Contact.jsx
+++ resdes/src/Contact/Contact.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import googleMail from '../Assets/gmail.png'
+import React from 'react'; 
 import linkd from '../Assets/linkedin.png'
+import styles from './style.module.css'
 export default function Contact(){
 
     return(
         <div className='contactAbout'>
             <div id="contactme">
             <h1 className='h1style'>Contact Me</h1>
-            <a href='Nicholas.a.lacapria@gmail.com '><img alt="Nicks Gmail"src={googleMail} />Nicholas.a.lacapria@gmail.com </a>
-            <h1 className='h1style'> or </h1>
+            <a id={styles.upwork} href='https://freelancerprofilenuxt.mesh.prod.platform.usw2.upwork/freelancers/~01d427babcfd8db6b1?mp_source=share'>upwork </a>
             
             <a href='https://www.linkedin.com/in/web-dev-nicholas-lacapria'>
-            <img alt="Nicks Linkedin"src={linkd} />linkedin.com/in/web-dev-nicholas-lacapria</a>
+            <img alt="Nicks Linkedin"src={linkd} /></a>
+            <a id={styles.fiverr} href='http://www.fiverr.com/s/EgZXR1e'>fiverr<span id={styles.fiverrp}>.</span></a>
         
             </div>
             </div>
